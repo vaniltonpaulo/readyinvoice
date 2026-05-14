fb69d1b8808905f7a50086904f2e0a615d568add
vaniltonpaulo <V.Paulo@campus.lmu.de>
2026-05-14 12:31:32 +0200

    Initial commit

---
+++ readyinvoice/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for readyinvoice project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'readyinvoice.settings')
+
+application = get_asgi_application()
